648e58aa0d1526287ce7adcf846c97836b153b32
Jose <josedfa86@gmail.com>
2018-02-17 13:23:22 +0100

    Merge branch 'Adrian' into Jose

--- app/src/main/java/com/example/admin/kakawev2/MainActivity.java
+++ app/src/main/java/com/example/admin/kakawev2/MainActivity.java
@@ -95,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
             };
         }, DURACION_SPLASH);*/
 
-        Intent intent = new Intent(this,RegisterActivity.class);
+        Intent intent = new Intent(this,LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
