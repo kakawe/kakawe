706c5d33d0b1d3da5ec1932af1dc728407e097dd
adrianmg89 <a.marcos.g989@gmail.com>
2018-02-19 18:10:41 +0100

    save lunes 19 febrero

---
+++ app/src/main/java/com/example/admin/kakawev2/Anuncio2.java
@@ -0,0 +1,95 @@
+package com.example.admin.kakawev2;
+
+/**
+ * Created by admin on 19/02/2018.
+ */
+
+public class Anuncio2 {
+
+    String correoAnunciante;
+    String titulo;
+    String tipo;
+    String categoria;
+    String foto;
+    String descripcion;
+    String fechaPublicacion;
+    String fechaCaducidad;
+
+    public Anuncio2() {
+    }
+
+    public Anuncio2(String correoAnunciante, String titulo, String tipo, String categoria, String foto, String descripcion, String fechaPublicacion, String fechaCaducidad) {
+        this.correoAnunciante = correoAnunciante;
+        this.titulo = titulo;
+        this.tipo = tipo;
+        this.categoria = categoria;
+        this.foto = foto;
+        this.descripcion = descripcion;
+        this.fechaPublicacion = fechaPublicacion;
+        this.fechaCaducidad = fechaCaducidad;
+    }
+
+    public String getCorreoAnunciante() {
+        return correoAnunciante;
+    }
+
+    public void setCorreoAnunciante(String correoAnunciante) {
+        this.correoAnunciante = correoAnunciante;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(String fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public String getFechaCaducidad() {
+        return fechaCaducidad;
+    }
+
+    public void setFechaCaducidad(String fechaCaducidad) {
+        this.fechaCaducidad = fechaCaducidad;
+    }
+}
